8c7ecbe9512cd153cdbcd84b8ec9e14b2dbf7158
dfcarrera79 <dfcarrera@outlook.com>
2023-05-09 23:06:06 -0500

    Se agregó actualizar estado

--- api/estados.py
+++ api/estados.py
@@ -1,16 +1,51 @@
 import json
+import datetime
 import fastapi
 from fastapi import Request
+from typing import Optional
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, text
+from fastapi.responses import JSONResponse
 
 router = fastapi.APIRouter()
+
+## Models
+class EstadoModel(BaseModel):
+  id_reclamo: int
+  estado: str
+  login_usuario: Optional[str]
+  nombre_usuario: Optional[str]
+  respuesta_finalizado: Optional[str]
 
 db_uri = "postgresql://postgres:01061979@localhost:5432/reclamos"
 engine = create_engine(db_uri)
 
 @router.put("/actualizar_estado")
-async def actualizar_estado(request: Request):
-  reclamo_str = await request.body()
-  reclamo = json.loads(reclamo_str)
-  print('[RECLAMO]: ' ,reclamo)
-  return reclamo
+async def actualizar_estado(data: EstadoModel):
+  id_reclamo = data.id_reclamo 
+  estado = data.estado
+  login_usuario = data.login_usuario
+  nombre_usuario = data.nombre_usuario
+  respuesta_finalizado = data.respuesta_finalizado
+  
+  sql = ""
+
+  if estado == "PEN":
+    sql = f"UPDATE reclamo SET estado='PEN', fecha_enproceso=NULL, login_usuario=NULL, nombre_usuario=NULL WHERE id_reclamo='{id_reclamo}' RETURNING id_reclamo"
+
+  if estado == "PRO":
+    fecha = datetime.datetime.now().isoformat()
+    sql = f"UPDATE reclamo SET estado='PRO', fecha_enproceso='{fecha}', login_usuario='{login_usuario}', nombre_usuario='{nombre_usuario}' WHERE id_reclamo='{id_reclamo}' RETURNING id_reclamo"
+
+  if estado == "FIN":
+    fecha = datetime.datetime.now().isoformat()
+    sql = f"UPDATE reclamo SET estado='FIN', fecha_finalizado='{fecha}', login_usuario='{login_usuario}', nombre_usuario='{nombre_usuario}', respuesta_finalizado='{respuesta_finalizado}' WHERE id_reclamo='{id_reclamo}' RETURNING id_reclamo"
+  
+  try: 
+    with Session(engine) as session:
+      rows = session.execute(text(sql)).fetchall()
+      session.commit()
+      return JSONResponse({"error": "N", "mensaje": "Estado actualizado exitosamente", "objetos": rows[0][0]})
+  except Exception as e:
+    return {"error": "S", "mensaje": str(e)}  
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from api import motivos, reclamos, archivos
+from api import motivos, reclamos, archivos, estados
 
 app = FastAPI()
 
 app.include_router(motivos.router)
 app.include_router(reclamos.router)
 app.include_router(archivos.router)
+app.include_router(estados.router)
