76842a710cfae93452a396956dbfc46ece8d28b6
dfcarrera79 <dfcarrera@outlook.com>
2023-05-26 22:54:44 -0500

    Se anadio los archivos de testing y variables de entorno

--- src/db_config.py
+++ src/db_config.py
@@ -1,2 +1,9 @@
-db_uri1 = "postgresql://postgres:01061979@192.168.50.10:5432/reclamos"
-db_uri2 = "postgresql://postgres:01061979@192.168.50.10:5432/apromed"
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the environment variables
+db_uri1 = os.getenv("DB_URI1")
+db_uri2 = os.getenv("DB_URI2")
